d289d33e86c1954eae5f4ce8094cb4316f7ea080
davud <davud.haci.2001@gmail.com>
2021-11-05 02:58:30 +0400

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeathStar star1 = new DeathStar();
+            star1.FireLaser();
+
+            IVehicle newCar = new Car();
+            newCar.Drive();
+            
+        }
+    }
+
+
+    abstract class SpaceStation
+    {
+        public abstract void FireLaser();
+
+    }
+    class DeathStar : SpaceStation
+    {
+        public override void FireLaser() => Console.WriteLine("Pew Pew");
+    }
+
+
+
+
+    interface IVehicle
+    {
+        public void Drive();
+    }
+
+    class Car : IVehicle
+    {
+
+        void IVehicle.Drive()
+        {
+            Console.WriteLine("car is Driving");
+        }
+    }
+
+}
